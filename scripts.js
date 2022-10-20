92f79252d654e4f67f57e9a767f604ab36cdca54
Mirik08 <mira.martiskova@gmail.com>
2022-10-20 18:48:20 +0200

    Team6
    Commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// When the user clicks on the button, scroll to the top of the document
+
+var pressedButton = document.getElementsByTagName("button-1")[0];
+pressedButton.addEventListener("click", function (event) {
+   alert("You have pressed the button.")
+})
+
